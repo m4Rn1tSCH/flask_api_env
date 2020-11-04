b0a30700c05f85157826bd10130b173cce68a26c
Bill <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-11-04 11:39:23 -0500

    cred deleted

--- ml_code/model_data/PostgreSQL_credentials.py
+++ ml_code/model_data/PostgreSQL_credentials.py
@@ -4,6 +4,6 @@ Credentials for the full Yodlee database
 
 YDB_name = "postgres"
 YDB_user = "envel_yodlee"
-YDB_password = "Bl0w@F1sh321"
-YDB_host = "envel-yodlee-datasource.c11nj3dc7pn5.us-east-2.rds.amazonaws.com"
+YDB_password = "XX"
+YDB_host = "XX"
 YDB_port = "5432"
